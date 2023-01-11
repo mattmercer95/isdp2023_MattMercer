ff384a9f850040d3ba05b7e79933ca4ec7149120
Matt Mercer <mattmercer95@gmail.com>
2023-01-11 10:23:48 -0400

    Moved stored procedures to 1 file

--- SQL Scripts/bulleseyedbStoredProcedures.sql
+++ SQL Scripts/bulleseyedbStoredProcedures.sql
@@ -1,9 +1,18 @@
 drop procedure if exists GetEmployeeInfo;
 DELIMITER //
-
 create procedure GetEmployeeInfo(in user varchar(32))
 BEGIN
 	Select employeeID, username, firstName, lastName, email, employee.active, locked, permissionLevel as position, name as site
     from employee inner join posn using(positionID) inner join site using(siteID)
     where username = user;
 END //
+DELIMITER ;
+
+drop procedure if exists GetPermissionsByID;
+DELIMITER //
+create procedure GetPermissionsByID(in id int)
+BEGIN
+	Select permissionID from user_permission
+    where employeeID = id;
+END //
+DELIMITER ;
