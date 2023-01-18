a2eb08039d5b5ad2edd877648994f70524ac860e
Matt Mercer <mattmercer95@gmail.com>
2023-01-17 20:28:03 -0400

    ResetPassword WIP

--- web/Scripts/resetPassword.js
+++ web/Scripts/resetPassword.js
@@ -1,6 +1,32 @@
 window.onload = function () {
     //load user into username field
     document.querySelector("#username").value = sessionStorage.getItem("resetPasswordUser");
+    document.querySelector("#togglePassword").addEventListener("click", toggleVisiblePassword);
+    document.querySelector("#togglePasswordConfirm").addEventListener("click", toggleVisiblePasswordConfirm);
+    document.querySelector("#cancel").addEventListener("click", cancel);
 };
+
+function toggleVisiblePassword(){
+    let password = document.querySelector("#password");
+    // toggle the type attribute
+    let type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+}
+function toggleVisiblePasswordConfirm(){
+    let password = document.querySelector("#passwordConfirm");
+    // toggle the type attribute
+    let type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+}
+function cancel(){
+    sessionStorage.setItem("resetPasswordUser", null);
+    window.location.href = "index.html";
+}
 
 
