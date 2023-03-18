6a4384dc8956e12f338a8505f3dcbfbb49b91c39
Matt Mercer <mattmercer95@gmail.com>
2023-03-18 01:23:41 -0300

    added Deliveries page

--- web/Scripts/deliveries.js
+++ web/Scripts/deliveries.js
@@ -1,9 +1,10 @@
 let currentEmployee;
+
 const idleDurationMins = 15;
-const redirectUrl = "index.html";
+const redirectUrl = "../index.html";
 let idleTimeout;
 
-window.onload = function () {
+window.onload = async function () {
     //set current employee global to the user that logged in
     currentEmployee = JSON.parse(sessionStorage.getItem("employeeInfo"));
     //load name and title on the nav bar
@@ -12,50 +13,24 @@ window.onload = function () {
             `${currentEmployee.firstName} ${currentEmployee.lastName}, 
             ${currentEmployee.position}`;
     nameTitleElement.innerHTML = nameTitle + ` - ${currentEmployee.site}`;
+    
     //add event for logout button
     document.querySelector("#logoutLink").addEventListener("click", logout);
+    
     //reset idleTimeouts for clicking, moving the mouse, or typing
     document.addEventListener('click', resetIdleTimeout, false);
     document.addEventListener('mousemove', resetIdleTimeout, false);
     document.addEventListener('keydown', resetIdleTimeout, false);
-    document.querySelector("#userAccounts").addEventListener('click', ()=>{
-        window.location.href = "Dashboard/UserAccounts.html";
-    });
-    document.querySelector("#locations").addEventListener('click', ()=>{
-        window.location.href = "Dashboard/ViewLocations.html";
-    });
-    document.querySelector("#orders").addEventListener('click', ()=>{
-        window.location.href = "Dashboard/ViewOrders.html";
-    });
-    document.querySelector("#inventory").addEventListener('click', ()=>{
-        window.location.href = "Dashboard/ViewInventory.html";
-    });
-    document.querySelector("#deliveries").addEventListener('click', ()=>{
-        window.location.href = "Dashboard/Deliveries.html";
-    });
     //initialize idle timeout
     resetIdleTimeout();
-    checkPermissions();
+
+    document.querySelector("#returnToDash").addEventListener('click', returnToDash);
+
 };
 
-function checkPermissions(){
-    let permissions = JSON.parse(sessionStorage.getItem("permissions"));
-    permissions.forEach((p)=>{
-        if(p === "READUSER"){
-            document.querySelector("#userAccounts").hidden = false;
-        }
-//        if(p === "CREATEREPORT"){
-//            document.querySelector("#reports").hidden = false;
-//        }
-        if(p === "VIEWORDERS" || p === "DELIVERY"){
-            document.querySelector("#orders").hidden = false;
-        }
-        if(p === "DELIVERY"){
-            document.querySelector("#deliveries").hidden = false;
-        }
-    });
+function returnToDash(){
+    window.location.href = "../dashboard.html";
 }
-
 function resetIdleTimeout(){
     //clears current timeout
     if(idleTimeout) clearTimeout(idleTimeout);
@@ -65,13 +40,11 @@ function resetIdleTimeout(){
 
 //removes current employee from session storage and redirects user to sign-in page
 async function logout(){
-    let url = `LogOutService/logout`;
+    let url = `../LogOutService/logout`;
     let resp = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(currentEmployee)
     });
     sessionStorage.setItem("employeeInfo", null);
-    window.location.href = "index.html";
+    window.location.href = "../index.html";
 }
-
-
